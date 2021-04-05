fc3cc85684eaab2e264ecf6b8a20242e023e9ae9
Scrying <None>
2021-04-05 11:06:43 -0700

    Formating

--- relaytoggle.ino
+++ relaytoggle.ino
@@ -2,28 +2,28 @@
 
 #define ARRAYSIZE(x) ((sizeof(x) / sizeof(*(x))))
 
-/* Declare Input Pins
+/*  Declare Input Pins
 `*
- * NUM_INPUTPINS must match the number of pins in
- * the array
+ *  NUM_INPUTPINS must match the number of pins in
+ *  the array
  * 
  */
 #define NUM_INPUTPINS 4
 int inputPins[NUM_INPUTPINS] = {A0, A1, A2, A3};
 
-/* Declare Output Pins
+/*  Declare Output Pins
  *
- * NUM_OUTPUTPINS must match the number of pins in
- * the array
+ *  NUM_OUTPUTPINS must match the number of pins in
+ *  the array
  * 
  */
 #define NUM_OUTPUTPINS 4 
 int outputPins[NUM_OUTPUTPINS] = {0, 1, 2, 3};
 
-/* Assign Input Pins to Buttons
+/*  Assign Input Pins to Buttons
  *
- * Assign input pins to buttons here in the form of:
- *     ezButton buttonName(inputPin)
+ *  Assign input pins to buttons here in the form of:
+ *      ezButton buttonName(inputPin)
  * 
  */
 ezButton b0(A0);
@@ -44,20 +44,20 @@ ezButton buttons[NUM_BUTTONS] = {b0, b1, b2, b3};
 /*  Initialize Button State  */
 uint8_t buttonStates[NUM_BUTTONS] = {LOW};
 
-/*  Set ezButton Debounce Time in MIlliseconds  */
+/*  Set ezButton Debounce Time in Milliseconds  */
 #define DEBOUNCE 50
 
 /*  Initialize Arduino Pins  */
 void setup() {
 
-    /* Initialize Output Pins  */
+    /*  Initialize Output Pins  */
     for (int i=0; i < ARRAYSIZE(outputPins); i++) {
 
         pinMode(outputPins[i], OUTPUT);
 
     }
 
-    /* Initialize ezButton Debounce Time in Milliseconds  */
+    /*  Initialize ezButton Debounce Time in Milliseconds  */
     for (int i=0; i < ARRAYSIZE(buttons); i++) {
 
         buttons[i].setDebounceTime(DEBOUNCE);
@@ -85,7 +85,7 @@ void loop() {
             digitalWrite(outputPins[i], buttonStates[i]);
 
         }
-        // Code End 
+        //  Code End 
     }
 
 }
